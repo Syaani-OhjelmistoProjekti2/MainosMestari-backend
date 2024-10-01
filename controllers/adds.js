fff5a38f2040b919d3d0489ccd86f951d73c12c9
Mikkosar <mikkosar12@gmail.com>
2024-10-01 13:32:43 +0300

    post method for stabilityAi added

--- controllers/adds.js
+++ controllers/adds.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 require('dotenv').config();
 const axios = require('axios');
 const FormData = require('form-data');
+const stabilityai = require('../utils/stabilityai')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -15,7 +16,7 @@ const storage = multer.diskStorage({
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         const filename = 'image-' + uniqueSuffix + path.extname(file.originalname);
         
-        req.fileFilename = filename;
+        req.filename = filename;
 
         cb(null, filename);
     }
@@ -26,7 +27,7 @@ const upload = multer({ storage: storage });
 addsRouter.post('/image', upload.single('img'), async (req, res) => {
     console.log("TOIMII");
 
-    const imgPath = req.fileFilename;
+    const imgPath = req.filename;
     console.log(imgPath)
     const prompt = req.body.prompt
     console.log(prompt)
@@ -34,6 +35,40 @@ addsRouter.post('/image', upload.single('img'), async (req, res) => {
 
     res.json(aiAnswer);
     await fs.unlinkSync(`controllers/uploads/${imgPath}`); 
+});
+
+addsRouter.post('/stabilityimg', upload.single('img'), async (req, res) => {
+    const imgPath = req.filename;
+    const prompt = req.body.prompt;
+
+    const stabilityimg = await stabilityai.stabilityimg({ prompt, imgPath });
+    const base64img = stabilityimg.data.toString('base64');
+    res.json({ data: base64img }); 
+
+    await fs.unlinkSync(`controllers/uploads/${imgPath}`); 
+})
+
+// Alla olevat on teistailuun, ei käytetä apissa
+
+addsRouter.get('/image', async (req, res) => {
+    console.log("TOIMII");
+    const aiAnswer = await openAi.openAiNewImg();
+
+    res.json(aiAnswer);
+});
+
+addsRouter.get('/describe', async (req, res) => {
+    console.log("TOIMII");
+    const aiAnswer = await openAi.describeImg();
+
+    res.json(aiAnswer);
+});
+
+addsRouter.get('/imagevariation', async (req, res) => {
+    console.log("TOIMII");
+    const aiAnswer = await openAi.imgVariation();
+
+    res.json(aiAnswer);
 });
 
 addsRouter.get('/stabilityimg', async (req, res) => {
@@ -63,28 +98,6 @@ addsRouter.get('/stabilityimg', async (req, res) => {
 
     const base64img = aiAnswer.data.toString('base64');
     res.json({ data:base64img });
-});
-
-
-addsRouter.get('/image', async (req, res) => {
-    console.log("TOIMII");
-    const aiAnswer = await openAi.openAiNewImg();
-
-    res.json(aiAnswer);
-});
-
-addsRouter.get('/describe', async (req, res) => {
-    console.log("TOIMII");
-    const aiAnswer = await openAi.describeImg();
-
-    res.json(aiAnswer);
-});
-
-addsRouter.get('/imagevariation', async (req, res) => {
-    console.log("TOIMII");
-    const aiAnswer = await openAi.imgVariation();
-
-    res.json(aiAnswer);
 });
 
 
